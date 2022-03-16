2fdaab2f43f11fd33596bf91f03a95c6d3c09419
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-03-16 16:08:56 +0100

    Writing frist test to test that a number will be returned when a number is entered, and creating method for fizz_buzz

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,3 @@
+def fizz_buzz(num)
+  return num # is return optional?
+end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,5 +1,9 @@
 require './lib/fizz_buzz'
 
 describe "fizz_buzz" do
-  # expectation
+  
+  it 'returns 1 if number is 1' do
+    expect(fizz_buzz(1)).to eq 1
+  end
+
 end
